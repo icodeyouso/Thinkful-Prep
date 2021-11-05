a569f47a2f5269d69c0e385b03dbcccbf4a059f7
icodeyouso <marquis.z.parks@gmail.com>
2021-11-04 23:03:33 -0700

    index.js

---
+++ index.js
@@ -0,0 +1,23 @@
+// don't modify these variables
+let saleTotal = 4.99;
+let costOfSale = 2.25;
+let profitOfSale = saleTotal - costOfSale;
+// don't modify these ones either
+let saleTotalMessage = "Sale netted us";
+let saleCostMessage = "Sale cost us";
+let profitMessage = "Profit from sale is";
+
+// example
+let formattedSaleTotal = saleTotalMessage + " " + saleTotal; //-> "Sale netted us 4.99"
+
+//your code below
+let formattedCostMessage; // 'Sale cost us 2.25'
+let formattedProfitMessage; // 'Profit from sale is 2.74'
+
+console.log(formattedSaleTotal);
+//console.log(formattedCostMessage);
+//console.log(formattedProfitMessage);
+
+
+// This tests your code - you can ignore it for now!
+//require('./test.js'); (void 0);
